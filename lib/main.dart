285a36a582bcb20d391155a471daebf2d18554b4
Matheus Timbó <matheusfild4@hotmail.com>
2024-03-09 15:22:18 -0300

    feat: removing .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:pilar_mobile_challenge/app/app.dart';
 import 'package:pilar_mobile_challenge/app/di.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env", isOptional: true);
   await initializeDateFormatting('pt_BR', null);
   DI.setup();
   runApp(const App());
--- lib/shared/utils/constants.dart
+++ lib/shared/utils/constants.dart
@@ -1,7 +1,5 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class Constants {
-  static String apiKey = dotenv.env['TMDB_API_KEY'] ??
+  static String apiKey =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjdlNzA3NjdkZmU0MmY4ZjcyY2Q4YjllNTkyYTQ0YyIsInN1YiI6IjVlNGM1NGJiOWI4NjE2MDAxODY3MDE3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cwXiGawvofBRNarR6abFHG_LxeEGrManFqQNEdZdKPk';
   static const apiBaseUrl = 'https://api.themoviedb.org/3';
   static const String tmdbLogo =
